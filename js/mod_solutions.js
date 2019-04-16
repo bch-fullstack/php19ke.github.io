ddfe00ca481bf09b019cfdfb9b3619f3958276f0
Tien Hoang Pham <tienhoang.pham@bc.fi>
2019-04-16 13:25:14 +0300

    Update mod_solutions.js
    prepend slick script to header

--- js/mod_solutions.js
+++ js/mod_solutions.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     var slickScript = document.createElement('script');
     slickScript.src = '//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js';
-    document.head.appendChild(slickScript);
+    $('head').prepend(slickScript);
 
     var slickStyle = document.createElement('link');
     slickStyle.href = '//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css';
