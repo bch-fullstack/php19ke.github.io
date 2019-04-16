234f3e25ed5047f1d3d94d0d2e4d45f31cf99b58
Tien Hoang Pham <tienhoang.pham@bc.fi>
2019-04-16 13:17:50 +0300

    Update mod_solutions.js
    add slick css

--- js/mod_solutions.js
+++ js/mod_solutions.js
@@ -2,4 +2,10 @@ $(document).ready(function(){
     var slickScript = document.createElement('script');
     slickScript.src = '//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js';
     document.head.appendChild(slickScript);
+
+    var slickStyle = document.createElement('link');
+    slickStyle.href = '//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css';
+    slickStyle.rel = 'stylesheet';
+    slickStyle.type = 'text/css';
+    document.head.appendChild(slickStyle);
 });
