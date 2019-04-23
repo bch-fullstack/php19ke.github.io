9bc4137e5500d1a8a0fd6fd2091ed6d771132806
Tien Hoang Pham <tienhoang.pham@bc.fi>
2019-04-23 14:06:57 +0300

    add form and create handleForm.php

--- js/mod_solutions.js
+++ js/mod_solutions.js
@@ -1,20 +1,4 @@
 $(document).ready(function(){
-    var slickScript = document.createElement('script');
-    slickScript.src = '//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js';
-    $('head').prepend(slickScript);
-
-    var slickStyle = document.createElement('link');
-    slickStyle.href = '//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css';
-    slickStyle.rel = 'stylesheet';
-    slickStyle.type = 'text/css';
-    document.head.appendChild(slickStyle);
-
-    $('body').append('<div class="your-class"><div>your content</div><div>your content</div><div>your content</div></div>');
-    setTimeout(function(){
-        $('.your-class').slick({
-            rtl: true
-        });
-    }, 1000);
-    
-          
+    var x = document.createElement('form');
+    document.body.appendChild(x);    
 });
