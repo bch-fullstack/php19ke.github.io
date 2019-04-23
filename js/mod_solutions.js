41ae88659839a1d90e15682cb26f640c775aa25d
Tien Hoang Pham <tienhoang.pham@bc.fi>
2019-04-23 14:11:27 +0300

    Update mod_solutions.js

--- js/mod_solutions.js
+++ js/mod_solutions.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
     var x = document.createElement('form');
+    var subjectField = document.createElement('input');
+    subjectField.type = 'text';
+    var submitForm = document.createElement('input');
+    submitForm.type = 'submit';
+    var textareaField = document.createElement('textarea');
+    
+    x.appendChild(subjectField);
+    x.appendChild(textareaField);
+    x.appendChild(submitForm);
+
     document.body.appendChild(x);    
 });
